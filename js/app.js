0bc4850ac76d7590c0a2f58f81527a490bcc9fec
Junaid <sj.shaikhjunaid@gmail.com>
2021-10-05 18:31:26 +0530

    add functionality

--- js/app.js
+++ js/app.js
@@ -11,3 +11,33 @@ I just told you! You've killed me! Fry! Quit doing the right thing, you jerk! Mi
   `Man braid celiac synth freegan readymade, pitchfork fam salvia waistcoat lomo bitters gentrify four loko. Pitchfork semiotics post-ironic vegan. Tofu meditation microdosing hashtag semiotics venmo. Flexitarian vape tilde taiyaki. Prism poutine farm-to-table, messenger bag vegan taxidermy tattooed sartorial squid jean shorts fixie selvage trust fund vape.`,
   `Rutters Plate Fleet boom chandler Brethren of the Coast handsomely lookout marooned brigantine knave. Buccaneer gangway jack rum loot spyglass line Jack Tar fore gaff. Gaff topmast scuttle ballast swab draught measured fer yer chains dance the hempen jig Chain Shot yardarm.`,
 ];
+
+const form = document.querySelector(".lorem-form");
+
+const amount = document.querySelector("#amount");
+
+const loremText = document.querySelector(".lorem-text");
+
+function generateParagraph(e) {
+  e.preventDefault();
+
+  const paragraphNumber = Number(amount.value);
+
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < paragraphNumber; i++) {
+    loremText.innerHTML = "";
+
+    let randomNumber = Math.floor(Math.random() * text.length);
+
+    let para = document.createElement("p");
+
+    para.append(text[randomNumber]);
+
+    fragment.append(para);
+  }
+
+  loremText.append(fragment);
+}
+
+form.addEventListener("submit", generateParagraph);
